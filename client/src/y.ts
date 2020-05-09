7fbedc96661aac7e6dfc3caae1897f97ac00d04f
inad9300 <inad9300@gmail.com>
2020-05-09 12:11:28 +0200

    Initial commit

---
+++ client/src/y.ts
@@ -0,0 +1,4 @@
+import type { N } from '../../server/src/x'
+
+let n: N = 42
+console.log(n)
---
+++ server/src/x.ts
@@ -0,0 +1,5 @@
+export type N = number
+
+function f() {
+  console.log("I should not be part of the client's compiled code.")
+}
